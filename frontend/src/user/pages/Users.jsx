69fad82415e409cbbc876650f3386aaeebec80e3
Elias Roussos <eliasisaiah@outlook.com>
2024-02-03 13:40:32 -0500

    added some prepared componenets

--- frontend/src/user/pages/Users.jsx
+++ frontend/src/user/pages/Users.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import UsersList from '../components/UsersList';
+import gween from '/gween.jpg';
 
 const Users = () => {
-    return <h2>Users works</h2>
+    const USERS = [
+        { id: 'u1', name: "mike schwarz", image: gween, places: 3 }
+    ]
+    return <UsersList items={USERS} />;
 };
 
 export default Users;
