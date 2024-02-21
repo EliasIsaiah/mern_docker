4d389dd3036141064c286a0a3348a2c0ef65c31e
Elias Roussos <eliasisaiah@outlook.com>
2024-02-21 16:58:44 -0500

    working on the backend

---
+++ server/controllers/places-controllers.js
@@ -0,0 +1,60 @@
+const HttpError = require("../models/http-error");
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "empire state building",
+    description: "one of the most famous sky scrapers in the world",
+    location: {
+      lat: 40.7484474,
+      lng: -73.9871516,
+    },
+    address: "20 W 34th St, New York, NY 10001",
+    creator: "u1",
+  },
+  {
+    id: "p2",
+    title: "willis tower",
+    description: "formerly the tallest building in chicago",
+    location: {
+      lat: 41.8788804,
+      lng: -87.6384898,
+    },
+    address: "233 S Wacker Dr, Chicago, IL 60606",
+    creator: "u1",
+  },
+  {
+    id: "p3",
+    title: "JPMorgan Chase McCoy Center",
+    description: "JPMC Corporate office, Columbus, Ohio",
+    location: {
+      lat: 40.1366769,
+      lng: -83.0179706,
+    },
+    address: "1111 Polaris Pkwy, Columbus, OH 43240",
+    creator: "u2",
+  },
+];
+
+const getPlaceById = (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id", 404);
+  }
+  res.json({ place });
+};
+
+const getPlaceByUser = (req, res, next) => {
+  const { uid } = req.params;
+  const places = DUMMY_PLACES.filter((place) => place.creator === uid);
+  if (!places.length > 0) {
+    return next(
+      new HttpError("could not find any places created by that user", 404)
+    );
+  }
+  res.json({ places });
+};
+
+exports.getPlaceById = getPlaceById;
+exports.getPlaceByUser = getPlaceByUser;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,29 @@
+require("dotenv").config();
+
 const express = require("express");
-const app = express();
+const bodyParser = require("body-parser");
+
 const port = process.env.PORT || 3000;
+const placesRoutes = require("./routes/places-routes");
+
+const app = express();
+
+app.use("/api/places/", placesRoutes);
+app.use((error, req, res, next) => {
+  if (res.headersSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred" });
+});
 
 const path = require("path");
 
 // Routes
 app.get("/test", (req, res) => {
-    res.send("hello world!");
+  res.send("hello world!");
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
